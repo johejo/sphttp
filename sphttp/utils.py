bcf06668f1a91f39413272b817acf1f87103ba6a
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-01-09 19:30:07 +0900

    fix init delay key

--- sphttp/utils.py
+++ sphttp/utils.py
@@ -1,7 +1,6 @@
 from urllib.parse import urlparse
 import asyncio
 import time
-from yarl import URL
 
 import requests
 from hyper import HTTP20Connection
@@ -50,7 +49,7 @@ def async_get_length(urls):
                     message = 'Host does not support Content-Length header.'
                     raise NoContentLength(message)
 
-                delays[URL(url).host] = time.monotonic() - begin
+                delays[url] = time.monotonic() - begin
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
