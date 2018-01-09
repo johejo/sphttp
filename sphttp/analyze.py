086f89eb15be191a0b10c5b7b588ad279a25f1d0
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-01-09 10:52:05 +0900

    add log about host

--- sphttp/analyze.py
+++ sphttp/analyze.py
@@ -1,12 +1,10 @@
 # Functions for log analysis
 
-# The log format is a tuple of time and received block ID.
+# The log format is a tuple of time, received block ID and hostname.
 
 # Example
 # An event log
-# (t, block_id)
-# List of log
-# recv_log = [(0, 0), (1, 2), (2, 1)]
+# (t, block_id, hostname)
 
 from statistics import mean, stdev
 import pickle
@@ -19,18 +17,35 @@ def open_log(filename):
     return send_log, recv_log
 
 
-def separate_log(log):
-    time_stamps = []
-    block_numbers = []
-    for t, bn in log:
-        time_stamps.append(t)
-        block_numbers.append(bn)
+def get_t_log(log):
+    return [t for t, _, _ in log]
 
-    return time_stamps, block_numbers
+
+def get_block_log(log):
+    return [b for _, b, _ in log]
+
+
+def get_host_log(log):
+    return [h for _, _, h in log]
+
+
+def separate_log(log):
+    return get_t_log(log), get_block_log(log), get_host_log(log)
+
+
+def separate_log_for_each_host(log):
+    host_log = get_host_log(log)
+    host = set(host_log)
+    r_log = {h: [] for h in host}
+
+    for t, b, h in log:
+        r_log[h].append((t, b))
+
+    return r_log
 
 
 def calc_num_staying_blocks(recv_log):
-    _, block_log = separate_log(recv_log)
+    block_log = get_block_log(recv_log)
 
     buf = []  # Buffer for block ID
     nsbib = []  # Number of staying blocks in buffer
@@ -58,7 +73,7 @@ def calc_num_staying_blocks(recv_log):
 
 
 def calc_num_simultaneous_return_block(recv_log):
-    _, block_log = separate_log(recv_log)
+    _, block_log, _ = separate_log(recv_log)
 
     buf = []  # Buffer for block ID
     rbi = 0  # Returned block ID
@@ -88,27 +103,27 @@ def calc_num_simultaneous_return_block(recv_log):
 
 
 def calc_good_put(recv_log):
-    t, bn = separate_log(recv_log)
+    t, bn, _ = separate_log(recv_log)
     return max(t) / max(bn)
 
 
 def calc_initial_buffering_time(recv_log):
-    t_log, block_log = separate_log(recv_log)
+    t_log, block_log, _ = separate_log(recv_log)
     finish_time = max(t_log)
     ave_interval = finish_time / len(recv_log)
 
-    initial_buffering = max([ti - (i * ave_interval) for i, (ti, _) in enumerate(sorted(recv_log, key=lambda x: x[1]))])
+    initial_buffering = max([ti - (i * ave_interval) for i, (ti, _, _) in enumerate(sorted(recv_log, key=lambda x: x[1]))])
 
     return initial_buffering
 
 
 def calc_ave_delay_time(recv_log):
-    t_log, block_log = separate_log(recv_log)
+    t_log, block_log, _ = separate_log(recv_log)
     finish_time = max(t_log)
     ave_arrival_desired_interval = finish_time / len(recv_log)
 
     d = []
-    for i, (ti, _) in enumerate(sorted(recv_log, key=lambda x: x[1])):
+    for i, (ti, _, _) in enumerate(sorted(recv_log, key=lambda x: x[1])):
         if ti > ave_arrival_desired_interval * i:
             d.append(ti - ave_arrival_desired_interval * i)
         else:
@@ -120,7 +135,7 @@ def calc_ave_delay_time(recv_log):
 
 
 def get_throughput(recv_log, filesize):
-    t_log, _ = separate_log(recv_log)
+    t_log = get_t_log(recv_log)
     end = max(t_log)
     thp = filesize * 8 / end / 10 ** 6
     return thp
