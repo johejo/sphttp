c0e77eef482114badd09a0125dc7a3564a47f0c5
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-02-01 07:42:39 +0900

    add build trigger to docker hub

--- scripts/docker_hub_rebuild.sh
+++ scripts/docker_hub_rebuild.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/johejo/sphttp-proxy/trigger/${DOCKER_HUB_TRIGGER_TOKEN}/
+curl -H "Content-Type: application/json" \
+     --data '{"build": true}' \
+     -X POST https://registry.hub.docker.com/u/johejo/sphttp-proxy/trigger/${DOCKER_HUB_TRIGGER_TOKEN}/
