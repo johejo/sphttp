e02f7649d1a0b7e280dacd4937e58160601d783f
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-02-14 23:07:57 +0900

    analyze function changed

--- sphttp/analyze.py
+++ sphttp/analyze.py
@@ -1,48 +1,52 @@
 # Functions for log analysis
 
 # The log format is a tuple of time, received block ID and hostname.
+# It is recommended that to read and execute "__test ()"
+# to check the behavior of each function.
 
-# Example
-# An event log
+# An event
 # (t, block_id, hostname)
 
-from statistics import mean, stdev
+# A log is list of events
+
+# sr_log means "send_log or recv_log"
+
+
+from statistics import mean, median
 import pickle
 from collections import deque
 
 
 def open_log(filename):
     with open(filename, 'rb') as f:
-        raw_log = pickle.load(f)
+        pickled = pickle.load(f)
 
-    return raw_log
-
-
-def get_t_log(log):
-    return [t for t, _, _ in log]
+    return pickled
 
 
-def get_block_log(log):
-    return [b for _, b, _ in log]
+def get_time_log(sr_log):
+    return [t for t, _, _ in sr_log]
 
 
-def get_host_log(log):
-    return [h for _, _, h in log]
+def get_block_log(sr_log):
+    return [b for _, b, _ in sr_log]
 
 
-def separate_log(log):
-    return get_t_log(log), get_block_log(log), get_host_log(log)
+def get_host_log(sr_log):
+    return [h for _, _, h in sr_log]
 
 
-def separate_log_for_each_host(log):
-    host_log = get_host_log(log)
-    host = set(host_log)
-    r_log = {h: [] for h in host}
+def separate_log(sr_log):
+    return get_time_log(sr_log), get_block_log(sr_log), get_host_log(sr_log)
 
-    for t, b, h in log:
-        r_log[h].append((t, b, h))
 
-    return r_log
+def separate_log_for_each_host(sr_log):
+    separated = {h: [] for h in set(get_host_log(sr_log))}
+
+    for t, b, h in sr_log:
+        separated[h].append((t, b, h))
+
+    return separated
 
 
 def get_invalid_block_log(recv_log):
@@ -68,35 +72,7 @@ def get_invalid_block_log(recv_log):
     return nsbib
 
 
-def calc_num_staying_blocks(recv_log):
-    block_log = get_block_log(recv_log)
-
-    buf = []  # Buffer for block ID
-    nsbib = []  # Number of staying blocks in buffer
-    rbi = 0  # Returned block ID
-
-    for block_id in block_log:
-        buf.append(block_id)
-        buf.sort()
-
-        i = 0
-        while i < len(buf):
-            if rbi == buf[i]:
-                buf.pop(i)
-                rbi += 1
-
-            else:
-                i += 1
-
-        nsbib.append(len(buf))
-
-    nsbib_mean = mean(nsbib)
-    nsbib_stdev = stdev(nsbib)
-
-    return nsbib_mean, nsbib_stdev
-
-
-def calc_num_simultaneous_return_block(recv_log):
+def get_num_simul_return_block_log(recv_log):
     _, block_log, _ = separate_log(recv_log)
 
     buf = []  # Buffer for block ID
@@ -120,58 +96,32 @@ def calc_num_simultaneous_return_block(recv_log):
 
         nsrb.append(rn)
 
-    nsrb_mean = mean(nsrb)
-    nsrb_stdev = stdev(nsrb)
-
-    return nsrb_mean, nsrb_stdev
+    return nsrb
 
 
-def calc_good_put(recv_log):
-    t, bn, _ = separate_log(recv_log)
-    return max(t) / max(bn)
+def calc_init_buffering_time(recv_log):
+    return max(get_delay_time_log(recv_log))
 
 
-def calc_initial_buffering_time(recv_log):
-    t_log, block_log, _ = separate_log(recv_log)
-    finish_time = max(t_log)
-    ave_itv = finish_time / len(recv_log)
+def get_delay_time_log(recv_log):
+    ideal_interval = max(get_time_log(recv_log)) / len(recv_log)
 
-    return max([t - (i * ave_itv)
-                for i, (t, _, _) in enumerate(sorted_by_block(recv_log))])
-
-
-def get_ave_delay_log(recv_log):
-    t_log, block_log, _ = separate_log(recv_log)
-    finish_time = max(t_log)
-    ave_itv = finish_time / len(recv_log)
-
-    return [t - ave_itv * i if t - ave_itv * i >= 0 else None
+    return [t - ideal_interval * i
             for i, (t, _, _) in enumerate(sorted_by_block(recv_log))]
-
-
-def calc_ave_delay_time(recv_log):
-    t_log, block_log, _ = separate_log(recv_log)
-    finish_time = max(t_log)
-    ave_itv = finish_time / len(recv_log)
-
-    return mean([t - ave_itv * i
-                 for i, (t, _, _) in enumerate(sorted_by_block(recv_log))
-                 if t - ave_itv * i >= 0])
 
 
 def sorted_by_block(sr_log):
     return sorted(sr_log, key=lambda x: x[1])
 
 
-def get_throughput(recv_log, filesize):
-    t_log = get_t_log(recv_log)
+def calc_goodput(recv_log, filesize, opt=10**6):
+    t_log = get_time_log(recv_log)
     end = max(t_log)
-    thp = filesize * 8 / end / 10 ** 6
+    thp = filesize * 8 / end / opt
     return thp
 
 
 def pick_dup_send(send_log):
-
     n = 0
     y = deque()
     for t, bi, _ in send_log:
@@ -180,22 +130,29 @@ def pick_dup_send(send_log):
         else:
             n += 1
         y.append((t, bi, _))
+
     return list(y)
 
 
-def calc_intervals(send_log, recv_log):
-    # return {sh: mean(rt - st)
-    #         for (st, _, sh), (rt, _, _) in
-    #         zip(sorted_by_block(send_log), sorted_by_block(recv_log))}
+def calc_avg_block_arr_interval(send_log, recv_log):
+    return {k: mean(v)
+            for k, v in get_block_arr_interval(send_log, recv_log).items()}
 
-    x = {h: [] for h in set([sh for _, _, sh in send_log])}
+
+def calc_med_block_arr_interval(send_log, recv_log):
+    return {k: median(v)
+            for k, v in get_block_arr_interval(send_log, recv_log).items()}
+
+
+def get_block_arr_interval(send_log, recv_log):
+    dic = {h: [] for h in set([sh for _, _, sh in send_log])}
 
     for (st, _, sh), (rt, _, _) in \
             zip(pick_dup_send(sorted_by_block(send_log)),
                 sorted_by_block(recv_log)):
-        x[sh].append(rt - st)
+        dic[sh].append(rt - st)
 
-    return {k: mean(v) for k, v in x.items()}
+    return dic
 
 
 # Test
@@ -211,12 +168,19 @@ def __test():
         (4, 5, 'hoge.com'), (5, 7, 'bar.com'), (6, 8, 'hoge.com'),
         (7, 3, 'hoge.com'), (8, 6, 'hoge.com'), (9, 4, 'hoge.com'),
     ]
-    print(calc_num_staying_blocks(recv_log))
-    print(calc_num_simultaneous_return_block(recv_log))
-    print(calc_good_put(recv_log))
-    print(calc_initial_buffering_time(recv_log))
-    print(calc_ave_delay_time(recv_log))
-    print(calc_intervals(send_log, recv_log))
+
+    print('SEND LOG')
+    print(send_log)
+
+    print('RECV LOG')
+    print(recv_log)
+
+    print(get_num_simul_return_block_log(recv_log))
+    print(calc_goodput(recv_log, 100))
+    print(calc_init_buffering_time(recv_log))
+    print(calc_avg_block_arr_interval(send_log, recv_log))
+    print(calc_med_block_arr_interval(send_log, recv_log))
+    print(get_block_arr_interval(send_log, recv_log))
 
 
 if __name__ == '__main__':
